509a7b8a8f4753a4b90a8a2f69208c667a9d63b1
sturdyspoon <316731+sturdyspoon@users.noreply.github.com>
2019-05-13 23:11:09 -0400

    misc

--- Assets/Bullseye/EllipseMesh.cs
+++ Assets/Bullseye/EllipseMesh.cs
@@ -12,6 +12,7 @@ public class EllipseMesh : MonoBehaviour
     public float speed = 1f;
     public Color color;
     public int quadCount = 120;
+    public float delayReverse;
 
     Mesh mesh;
     List<Vector3> vertices;
@@ -36,10 +37,11 @@ public class EllipseMesh : MonoBehaviour
 
     float maxAngle = (Mathf.PI * 2f);
     float lastT;
+    float reverseStart = Mathf.Infinity;
 
     void Update()
     {
-        if (lastT < maxAngle)
+        if (lastT < 1)
         {
             float t = Mathf.Clamp01((Time.time - delay) * speed);
             t = EasingFunction.EaseInOutSine(0, 1, t);
@@ -72,6 +74,24 @@ public class EllipseMesh : MonoBehaviour
             mesh.triangles = triangles.ToArray();
 
             lastT = t;
+        }
+        else if (reverseStart == Mathf.Infinity)
+        {
+            reverseStart = Time.time + delayReverse;
+        }
+
+        if (reverseStart < Time.time)
+        {
+            float t = Mathf.Clamp01((Time.time - reverseStart) * speed);
+            t = EasingFunction.EaseInOutSine(1, 0, t);
+            int target = Mathf.FloorToInt(t * quadCount) + 2;
+
+            vertices.RemoveRange(target, vertices.Count - target);
+            triangles.RemoveRange((target - 2) * 3, triangles.Count - ((target - 2) * 3));
+
+            mesh.Clear();
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles.ToArray();
         }
     }
 }
