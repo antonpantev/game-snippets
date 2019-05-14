f9a493585c3e28459b62f312b69ecae8089985e7
sturdyspoon <316731+sturdyspoon@users.noreply.github.com>
2019-05-13 22:50:05 -0400

    Renamed

--- Assets/Bullseye/Bullseye.cs
+++ Assets/Bullseye/Bullseye.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Foo : MonoBehaviour
+public class Bullseye : MonoBehaviour
 {
     public GameObject prefab;
     public Color[] colors;
