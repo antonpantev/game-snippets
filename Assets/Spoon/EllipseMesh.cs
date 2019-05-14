dca9d9565daac36b8352c3950c260517d4e733eb
sturdyspoon <316731+sturdyspoon@users.noreply.github.com>
2019-05-13 22:06:32 -0400

    Trying a thing

---
+++ Assets/Spoon/EllipseMesh.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EllipseMesh : MonoBehaviour
+{
+    public float xRadius = 2f / 3f;
+    public float yRadius = 1f;
+    public float innerRadius = 0f;
+    public float outerRadius = 1f;
+    public float delay;
+    public float speed = 3f;
+    public Color color;
+
+    Mesh mesh;
+    List<Vector3> vertices;
+    List<int> triangles;
+
+    void Start()
+    {
+        mesh = new Mesh();
+
+        MeshFilter mf = GetComponent<MeshFilter>();
+        mf.mesh = mesh;
+
+        vertices = new List<Vector3>();
+        vertices.Add(new Vector3(innerRadius, 0, 0));
+        vertices.Add(new Vector3(outerRadius, 0, 0));
+
+        triangles = new List<int>();
+
+        Material m = GetComponent<MeshRenderer>().material;
+        m.color = color;
+    }
+
+    float maxAngle = (Mathf.PI * 2f);
+    float lastT;
+
+    void Update()
+    {
+        if (lastT < maxAngle)
+        {
+            int i = vertices.Count - 2;
+
+            float t = Mathf.Clamp((Time.time - delay) * speed, 0f, maxAngle);
+
+            Vector3 p1 = new Vector3(Mathf.Cos(t) * xRadius, Mathf.Sin(t) * yRadius, 0.0f) * innerRadius;
+            vertices.Add(p1);
+
+            Vector3 p2 = new Vector3(Mathf.Cos(t) * xRadius, Mathf.Sin(t) * yRadius, 0.0f) * outerRadius;
+            vertices.Add(p2);
+
+            triangles.Add(i);
+            triangles.Add(i + 2);
+            triangles.Add(i + 1);
+
+            triangles.Add(i + 2);
+            triangles.Add(i + 3);
+            triangles.Add(i + 1);
+
+            mesh.Clear();
+            mesh.vertices = vertices.ToArray();
+            mesh.triangles = triangles.ToArray();
+
+            lastT = t;
+        }
+    }
+}
---
+++ Assets/Spoon/Spoon.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spoon : MonoBehaviour
+{
+    public Color c1 = Color.yellow;
+    public Color c2 = Color.red;
+    public float speed = 1f;
+
+    LineRenderer lr;
+    int lengthOfLineRenderer;
+
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+
+        float alpha = 1.0f;
+        Gradient gradient = new Gradient();
+        gradient.SetKeys(
+            new GradientColorKey[] { new GradientColorKey(c1, 0.0f), new GradientColorKey(c2, 1.0f) },
+            new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
+        );
+        lr.colorGradient = gradient;
+    }
+
+    float maxAngle = (Mathf.PI * 2f);
+    float lastT;
+
+    void Update()
+    {
+        if (lastT < maxAngle)
+        {
+            float t = Mathf.Clamp(Time.time * speed, 0f, maxAngle);
+            Vector3 point = new Vector3(Mathf.Cos(t), Mathf.Sin(t), 0.0f);
+            lr.positionCount++;
+            lr.SetPosition(lr.positionCount - 1, point);
+            lastT = t;
+        }
+    }
+}
