a879cb893241ca46be255b1be831290b45c5ed0e
sturdyspoon <316731+sturdyspoon@users.noreply.github.com>
2019-05-17 22:58:12 -0400

    Repeat spawn.

--- Assets/Existential/ExistentialSpawner.cs
+++ Assets/Existential/ExistentialSpawner.cs
@@ -6,6 +6,7 @@ public class ExistentialSpawner : MonoBehaviour
     public int minCount;
     public int maxCount;
     public float size;
+    public float tooCloseToCamera;
 
     public Vector2 delayStart;
     public Vector2 waitTime;
@@ -13,16 +14,25 @@ public class ExistentialSpawner : MonoBehaviour
 
     void Start()
     {
+        InvokeRepeating("Spawn", 0, delayStart.y + waitTime.y + deathTime.y);
+    }
+
+    void Spawn()
+    {
         int count = Random.Range(minCount, maxCount);
 
         for (int i = 0; i < count; i++)
         {
-            Existential prefab = prefabs[Random.Range(0, prefabs.Length)];
             Vector3 pos = Random.insideUnitSphere * size;
-            Existential e = Instantiate(prefab, pos, Random.rotation);
-            e.delayStart = Random.Range(delayStart.x, delayStart.y);
-            e.waitTime = Random.Range(waitTime.x, waitTime.y);
-            e.deathTime = Random.Range(deathTime.x, deathTime.y);
+            
+            if (Vector3.Distance(pos, Camera.main.transform.position) > tooCloseToCamera)
+            {
+                Existential prefab = prefabs[Random.Range(0, prefabs.Length)];
+                Existential e = Instantiate(prefab, pos, Random.rotation);
+                e.delayStart = Random.Range(delayStart.x, delayStart.y);
+                e.waitTime = Random.Range(waitTime.x, waitTime.y);
+                e.deathTime = Random.Range(deathTime.x, deathTime.y);
+            }
         }
     }
 }
