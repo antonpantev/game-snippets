f7a7b37ada64536beb66ad174067844e1ed1a774
sturdyspoon <316731+sturdyspoon@users.noreply.github.com>
2019-05-17 22:01:26 -0400

    Script to control animator.

---
+++ Assets/Existential/Existential.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Existential : MonoBehaviour
+{
+    public float delayStart = 1f;
+    public float waitTime = 1f;
+    public float deathTime = 1f;
+
+    Animator animator;
+    float startTime;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        Invoke("Appear", delayStart);
+        Invoke("Disappear", delayStart + waitTime);
+        Invoke("Die", delayStart + waitTime + deathTime);
+    }
+
+    void Appear()
+    {
+        animator.SetBool("appear", true);
+    }
+
+    void Disappear()
+    {
+        animator.SetBool("appear", false);
+        animator.SetBool("disappear", true);
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
