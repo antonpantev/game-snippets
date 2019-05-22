6b3a94070aae0860a7429318137957486e137fdf
sturdyspoon <316731+sturdyspoon@users.noreply.github.com>
2019-05-22 18:33:13 -0400

    seal

---
+++ Assets/Seal/Seal.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Seal : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("activate");
+        }
+    }
+}
