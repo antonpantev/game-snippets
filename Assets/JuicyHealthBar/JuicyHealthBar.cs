a7ffe8162eb0733cf6d878bff723414d9eb5f569
sturdyspoon <316731+sturdyspoon@users.noreply.github.com>
2019-06-11 12:45:43 -0400

    Updating Unity. Working on a juicy health bar.

---
+++ Assets/JuicyHealthBar/JuicyHealthBar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class JuicyHealthBar : MonoBehaviour
+{
+    public Transform bar;
+    public float currentHealth = 100f;
+    public float maxHealth = 100f;
+
+    float maxWidth;
+
+    void Start()
+    {
+        maxWidth = bar.localScale.x;
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            currentHealth -= Random.Range(0.05f, 0.30f) * maxHealth;
+            currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+        }
+
+        UpdateBar();
+    }
+
+    void UpdateBar()
+    {
+        Vector3 scale = bar.localScale;
+        scale.x = (currentHealth / maxHealth) * maxWidth;
+        bar.localScale = scale;
+    }
+}
