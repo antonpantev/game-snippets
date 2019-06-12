fab8ef9b036f324a4dde83303d504c7270ffdf45
sturdyspoon <316731+sturdyspoon@users.noreply.github.com>
2019-06-12 00:04:32 -0400

    Bottom bar.

--- Assets/JuicyHealthBar/JuicyHealthBar.cs
+++ Assets/JuicyHealthBar/JuicyHealthBar.cs
@@ -2,23 +2,28 @@
 
 public class JuicyHealthBar : MonoBehaviour
 {
-    public Transform bar;
+    public Transform bottomBar;
+    public Transform topBar;
     public float currentHealth = 100f;
     public float maxHealth = 100f;
+    public float bottomBarSpeed = 1f;
+    public float bottomBarWaitTime = 0.2f;
 
+    float bottomBarStartTime = Mathf.Infinity;
     float maxWidth;
 
     void Start()
     {
-        maxWidth = bar.localScale.x;
+        maxWidth = topBar.localScale.x;
     }
 
     void Update()
     {
         if (Input.GetButtonDown("Jump"))
         {
-            currentHealth -= Random.Range(0.05f, 0.30f) * maxHealth;
+            currentHealth -= Random.Range(0.05f, 0.1f) * maxHealth;
             currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+            bottomBarStartTime = Time.time + bottomBarWaitTime;
         }
 
         UpdateBar();
@@ -26,8 +31,17 @@ public class JuicyHealthBar : MonoBehaviour
 
     void UpdateBar()
     {
-        Vector3 scale = bar.localScale;
-        scale.x = (currentHealth / maxHealth) * maxWidth;
-        bar.localScale = scale;
+        float target = (currentHealth / maxHealth) * maxWidth;
+
+        Vector3 scale = topBar.localScale;
+        scale.x = target;
+        topBar.localScale = scale;
+
+        if (bottomBarStartTime < Time.time)
+        {
+            scale = bottomBar.localScale;
+            scale.x = Mathf.MoveTowards(scale.x, target, Time.deltaTime * bottomBarSpeed);
+            bottomBar.localScale = scale;
+        }
     }
 }
