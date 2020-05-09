36452ecab8ea48aa44961f50ad65a3e83f915ac0
sturdyspoon <316731+sturdyspoon@users.noreply.github.com>
2020-05-09 17:44:58 -0400

    Basic ring

---
+++ ring-outline/Assets/Scripts/Ring.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+    public float size = 2f;
+    public int count = 10;
+    public Transform prefab;
+
+    void Start()
+    {
+        float deltaAngle = (360f / count) * Mathf.Deg2Rad;
+
+        for (int i = 0; i < count; i++)
+        {
+            float x = Mathf.Cos(i * deltaAngle) * size;
+            float z = Mathf.Sin(i * deltaAngle) * size;
+
+            Instantiate(prefab, new Vector3(x, z, 0f), Quaternion.identity);
+        }
+    }
+}
---
+++ ring-outline/Assets/Scripts/Tangent.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Tangent : MonoBehaviour
+{
+    public float speed = 1f;
+
+    MeshRenderer mr;
+    float t = 0;
+    float startTime;
+
+    void Start()
+    {
+        mr = GetComponent<MeshRenderer>();
+
+        startTime = Time.time + 1f;
+    }
+
+    void Update()
+    {
+        if (Time.time > startTime)
+        {
+            mr.enabled = true;
+
+            Vector3 scale = transform.localScale;
+            scale.y = Mathf.SmoothStep(0, 1, t);
+            transform.localScale = scale;
+
+            if (t < 1)
+            {
+                t += speed * Time.deltaTime;
+            }
+            else
+            {
+                startTime = Time.time + 3f;
+                t = 0;
+            }
+        }
+    }
+}
